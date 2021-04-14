a5f9f352ed22fe9bb98310678d9f7a2853de249c
programacion <>
2021-04-14 13:57:16 +0200

    fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,8 +2,14 @@
 public class Fibonacci {
 
 	public int calculaTerminoFibonacci(int termino) {
-		assert termino>0;
-		//TODO 
-		return -1;
+		assert termino >= 0;
+		int retornado = 0;
+		// Caso base
+		if (termino == 0 || termino == 1) {
+			retornado = termino;
+		} else {
+			retornado = calculaTerminoFibonacci(termino - 2) + calculaTerminoFibonacci(termino - 1);
+		}
+		return retornado;
 	}
 }
